f6761ad73c8083eebc3afd09ed06deda88490a8a
dionware1 <dionicans@gmail.com>
2026-04-25 20:10:49 +0300

    Refactor matchmaking_dll namespace structure

--- matchmaking_dll.hpp
+++ matchmaking_dll.hpp
@@ -1,3 +1,17 @@
-namespace matchmaking_dll {
-    constexpr std::ptrdiff_t dwGameTypes = 0x1b8000;
+// Generated using https://github.com/a2x/cs2-dumper
+// 2026-04-25 20:06:00 UTC
+
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+
+namespace cs2_dumper {
+    namespace schemas {
+        namespace matchmaking_dll {
+            namespace matchmaking_dll {
+                constexpr std::ptrdiff_t dwGameTypes = 0x1B8000;
+            }
+        }
+    }
 }
