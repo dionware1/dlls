e1dbb5d889d65252357bd221ad43b9721b555783
dionware1 <dionicans@gmail.com>
2026-04-25 20:10:11 +0300

    Update inputsystem_dll.hpp

--- inputsystem_dll.hpp
+++ inputsystem_dll.hpp
@@ -1,3 +1,17 @@
-namespace inputsystem_dll {
-    constexpr std::ptrdiff_t dwInputSystem = 0x45ad0;
+// Generated using https://github.com/a2x/cs2-dumper
+// 2026-04-25 20:06:00 UTC
+
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+
+namespace cs2_dumper {
+    namespace schemas {
+        namespace inputsystem_dll {
+            namespace inputsystem_dll {
+                constexpr std::ptrdiff_t dwInputSystem = 0x45ad0;
+            }
+        }
+    }
 }
